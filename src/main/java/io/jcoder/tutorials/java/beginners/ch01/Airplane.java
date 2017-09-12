bc9ebb8429e6481990970490695e76f0d73be30d
camilojc <camilo@jcoder.io>
2017-09-12 20:26:59 +0100

    first commit of the example code for Chapter 1

---
+++ src/main/java/io/jcoder/tutorials/java/beginners/ch01/Airplane.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+package io.jcoder.tutorials.java.beginners.ch01;
+
+/**
+ * Basic example of a class representing an Airplane.
+ */
+public class Airplane {
+    private String manufacturer;
+
+    private int numberOfSeats;
+
+    private boolean landed;
+
+    private boolean gearDeployed;
+
+    public void setManufacturer(String newManufacturer) {
+        manufacturer = newManufacturer;
+    }
+
+    public void setNumberOfSeats(int newNumberOfSeats) {
+        numberOfSeats = newNumberOfSeats;
+    }
+
+    public void takeOff() {
+        landed = false;
+    }
+
+    public void land() {
+        landed = true;
+    }
+
+    public void deployLandingGear() {
+        gearDeployed = true;
+    }
+
+    public void retractLandingGear() {
+        gearDeployed = false;
+    }
+
+    public void printDetails() {
+        System.out.println("manufacturer=" + manufacturer + ", numberOfSeats=" + numberOfSeats + ", landed=" + landed
+                + ", gearDeployed=" + gearDeployed);
+    }
+
+}
---
+++ src/main/java/io/jcoder/tutorials/java/beginners/ch01/AirplaneApplication.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+package io.jcoder.tutorials.java.beginners.ch01;
+
+public class AirplaneApplication {
+    public static void main(String[] args) {
+        Airplane plane1 = new Airplane();
+        
+        plane1.setManufacturer("Boeing");
+        plane1.setNumberOfSeats(270);
+        plane1.printDetails();
+        
+        Airplane plane2 = plane1;
+        plane2.printDetails();
+        
+        Airplane plane3 = new Airplane();
+        plane3.setManufacturer("Airbus");
+        plane3.setNumberOfSeats(300);
+        plane3.deployLandingGear();
+        plane3.land();
+        plane3.printDetails();
+        
+        System.out.println("plane1 is pointing to the same instance as plane2: " + (plane1 == plane2));
+        System.out.println("plane1 is pointing to the same instance as plane3: " + (plane1 == plane3));
+    }
+}
