262913a86ed8192d3aa8c4e3f59844d6e49028fd
camilojc <camilo@jcoder.io>
2017-09-16 11:14:25 +0100

    basic example application

---
+++ src/main/java/ExampleApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+
+/**
+ * An Example java application to show the usage of javac and java
+ */
+public class ExampleApplication {
+    
+    /**
+     * Entry-point to our application
+     */
+    public static void main(String[] args) {
+        System.out.println("This is an example application.");
+    }
+    
+}
