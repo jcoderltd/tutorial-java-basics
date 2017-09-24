0fc235ccfcdb2473f3342792ed1e311792b3265f
camilojc <camilo@jcoder.io>
2017-09-24 19:18:56 +0100

    added example classes for the dot operator section

---
+++ src/main/java/io/jcoder/tutorials/java/beginners/ch01/dotoperator/Door.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+package io.jcoder.tutorials.java.beginners.ch01.dotoperator;
+
+/**
+ * Example Door class used in the dot (.) operator section.
+ * 
+ * @author Camilo Gonzalez
+ */
+public class Door {
+    public boolean locked = false;
+
+    public void lockDoor() {
+        locked = true;
+    }
+
+    public void unlockDoor() {
+        locked = false;
+    }
+}
---
+++ src/main/java/io/jcoder/tutorials/java/beginners/ch01/dotoperator/DoorApp.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+package io.jcoder.tutorials.java.beginners.ch01.dotoperator;
+
+/**
+ * Example DoorApp class used in the dot (.) operator section.
+ * 
+ * @author Camilo Gonzalez
+ */
+public class DoorApp {
+
+    public static void main(String[] args) {
+        Door door = new Door();
+
+        // invoking methods
+        door.lockDoor();
+
+        door.unlockDoor();
+
+        // accessing fields (read)
+        boolean isDoorLocked = door.locked;
+
+        // accessing fields (write)
+        door.locked = true;
+
+        System.out.println("isDoorLocked variable = " + isDoorLocked);
+        System.out.println("door.locked field = " + door.locked);
+    }
+
+}
