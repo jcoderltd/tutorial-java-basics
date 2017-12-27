8d18dd0ad3bbf06a5bad6f7822f049c892d800d7
camilojc <camilo@jcoder.io>
2017-12-27 15:02:55 -0500

    updated operators examples

---
+++ src/main/java/io/jcoder/tutorials/ch02/operators/JavaArithmeticOperatorsApp.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 - JCoder Ltd
+ */
+package io.jcoder.tutorials.ch02.operators;
+
+/**
+ * Example class used in the Arithmetics Operators section of the Java for Beginners Course.
+ * 
+ * <p>
+ * https://jcoder.io/article/java/beginners/ch02/operators
+ * </p>
+ * 
+ * @author Camilo Gonzalez
+ */
+public class JavaArithmeticOperatorsApp {
+
+    public static void main(String[] args) {
+        int cores = 4;
+        int memoryInGB = 256;
+
+        // The + symbol is used for addition
+        int result1 = memoryInGB + 256;
+        // result1 is 512
+        System.out.println(memoryInGB + " + 256 = " + result1);
+
+        // The - symbol is used for subtraction
+        int result2 = cores - 3;
+        // result2 is 1
+        System.out.println(cores + " - 3 = " + result2);
+
+        // The * symbol is used for multiplication
+        int result3 = cores * 2;
+        // result3 is 8
+        System.out.println(cores + " * 2 = " + result3);
+
+        // The / symbol is used for division
+        int result4 = cores / 2;
+        // result4 is 2
+        System.out.println(cores + " / 2 = " + result4);
+
+        // The % symbol is used to get the remainder of a division
+        int result5 = cores % 3;
+        // result5 is 1
+        System.out.println(cores + " % 3 = " + result5);
+    }
+}
---
+++ src/main/java/io/jcoder/tutorials/ch02/operators/JavaUnaryOperatorsApp.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 - JCoder Ltd
+ */
+package io.jcoder.tutorials.ch02.operators;
+
+/**
+ * Example class used in the Unary Operators section of the Java for Beginners Course.
+ * 
+ * <p>
+ * https://jcoder.io/article/java/beginners/ch02/operators
+ * </p>
+ * 
+ * @author Camilo Gonzalez
+ */
+public class JavaUnaryOperatorsApp {
+
+    public static void main(String[] args) {
+        // + and - as unary operators
+        int result1 = +1;
+        // result1 is 1 using either statement
+        System.out.println("result1 = " + result1);
+        
+        int result2 = -result1;
+        // result2 is -1
+        System.out.println("result2 = " + result2);
+        
+        // prefix increment operator
+        int value1 = 0;
+        int result3 = ++value1;
+        // result3 is 1 and value1 is also 1
+        System.out.println("result3 = " + result3);
+        System.out.println("value1 = " + value1);
+        
+        // postfix increment operator
+        int value2 = 0;
+        int result4 = value2++;
+        // result4 is 0 and value2 is 1
+        System.out.println("result4 = " + result4);
+        System.out.println("value2 = " + value2);
+        
+        // logical complement operator
+        boolean value3 = true;
+        boolean result5 = !value3;
+        System.out.println("value3 = " + value3);
+        System.out.println("result5 = " + result5);
+        
+    }
+}
