37c384b7778ce71e8fabb7c7024c5f7651b2972a
camilojc <camilo@jcoder.io>
2018-04-12 22:48:36 -0500

    added basic if-else statement example app

---
+++ src/main/java/io/jcoder/tutorials/ch03/controlflow/BasicIfElseStatementApp.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 - JCoder Ltd
+ */
+package io.jcoder.tutorials.ch03.controlflow;
+
+/**
+ * Basic example used in the If-Else Statement section of the Java for Beginners Course.
+ * 
+ * <p>
+ * https://jcoder.io/article/java/beginners/ch03/ifelsestatement
+ * </p>
+ * 
+ * @author Camilo Gonzalez
+ */
+public class BasicIfElseStatementApp {
+
+    public static void main(String[] args) {
+        int age = 19;
+        double discount = 1d;
+
+        if (age < 18) {
+            discount = 0.5d;
+            System.out.println("The age is under 18");
+        } else {
+            discount = 0d;
+            System.out.println("The age is 18 or over");
+        }
+
+        System.out.println("The discount to use is: " + discount);
+    }
+
+}
